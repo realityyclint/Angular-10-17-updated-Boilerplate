203ad970c8c4f0230f8c58bb63be1ad2e3af6df4
Rodriguez1718 <krisjohn.rodriguez@gmail.com>
2025-05-14 01:46:54 +0800

    updated url to connect to the backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://angular-10-17-updated-boilerplate.onrender.com'
+    apiUrl: 'https://node-js-mysql-boilerplate-api.onrender.com'
 };
