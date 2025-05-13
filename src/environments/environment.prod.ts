6e12d23907bef57131b5835580097c7ea7654918
Rodriguez1718 <krisjohn.rodriguez@gmail.com>
2025-05-14 01:25:09 +0800

    updated environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://localhost:4000'
+    apiUrl: 'postgresql://postgres_db_h0si_user:qVjh150QOJtypvVfR6ySlCqkw2m0cevN@dpg-d0hms6euk2gs73eoks0g-a.oregon-postgres.render.com/postgres_db_h0si'
 };
