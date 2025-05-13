fb55451cd81bbc0604226b55bc0d7efb1cf60d95
Rodriguez1718 <krisjohn.rodriguez@gmail.com>
2025-05-14 01:41:00 +0800

    updated environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'postgresql://postgres_db_h0si_user:qVjh150QOJtypvVfR6ySlCqkw2m0cevN@dpg-d0hms6euk2gs73eoks0g-a.oregon-postgres.render.com/postgres_db_h0si'
+    apiUrl: 'https://angular-10-17-updated-boilerplate.onrender.com'
 };
